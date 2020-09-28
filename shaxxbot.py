b30a6c85011505468cb25bcf6b658548f182ccc9
AntErs0 <71837343+AntErs0@users.noreply.github.com>
2020-09-28 14:40:05 +0200

    Update shaxxbot.py

--- shaxxbot.py
+++ shaxxbot.py
@@ -3,6 +3,7 @@ import os
 import discord
 import random
 from dotenv import load_dotenv
+from discord import ext
 from discord.ext import commands
 
 load_dotenv()
@@ -12,19 +13,19 @@ bot = commands.Bot(command_prefix='Shaxx ')
 
 @bot.event
 async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+	print(f'{bot.user} has connected to Discord!')
 
 @bot.command(name='positivo', help='Lord Shaxx è fiero di te',pass_context=True)
 async def positivo(context):
-	paudiofile = os.scandir('audio/positive/')
+	paudiofile = os.listdir('audio/positive/')
 	pselectedaudio = random.choice(paudiofile)
 	user=context.message.author
-	voice_channel=user.voice.voice_channel
+	voice_channel=user.voice.channel
 	channel=None
 	if voice_channel!= None:
 		channel=voice_channel.name
-		await client.say('User is in channel: '+ channel)
-		vc= await client.join_voice_channel(voice_channel)
+		await context.send('User is in channel: '+ channel)
+		vc= await channel.connect(voice_channel)
 		player = vc.create_ffmpeg_player(pselectedaudio, after=lambda: print('done'))
 		player.start()
 		while not player.is_done():
@@ -32,6 +33,6 @@ async def positivo(context):
 			player.stop()
 		await vc.disconnect()
 	else:
-		await client.say('Non sei in un canale vocale!')
+		await context.send('Non sei in un canale vocale!')
 
 bot.run('NzU5MDkwOTg3NTYwODYxNjk4.X24ckg.2ucH0GCirkLgT5wpJs-GTDWsnME')
