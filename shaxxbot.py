1fcbfefccdc763664a1b884d8382d4be306b0b7b
AntErs0 <71837343+AntErs0@users.noreply.github.com>
2020-09-28 13:19:59 +0200

    Update shaxxbot.py

--- shaxxbot.py
+++ shaxxbot.py
@@ -1,39 +1,35 @@
 # bot.py
 import os
-
 import discord
-from dotenv import load_dotenv
 import random
+from dotenv import load_dotenv
 from discord.ext import commands
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
-
-
+bot = commands.Bot(command_prefix='Shaxx ')
 
 @bot.event
 async def on_ready():
 	print(f'{client.user} has connected to Discord!')
-    
-@bot.event
-async def on_message(message):
-	if message.author == client.user:
-		return
 
-	brooklyn_99_quotes = [
-		'I\'m the human form of the 💯 emoji.',
-		'Bingpot!',
-		(
-		'Cool. Cool cool cool cool cool cool cool, '
-		'no doubt no doubt no doubt no doubt.'
-		),
-	]
+@bot.command(name='positivo', help='Lord Shaxx è fiero di te',pass_context=True)
+async def positivo(context):
+    user=context.message.author
+    voice_channel=user.voice.voice_channel
+    channel=None
+    if voice_channel!= None:
+        channel=voice_channel.name
+        await client.say('User is in channel: '+ channel)
+        vc= await client.join_voice_channel(voice_channel)
+        player = vc.create_ffmpeg_player('vuvuzela.mp3', after=lambda: print('done'))
+        player.start()
+        while not player.is_done():
+            await asyncio.sleep(1)
+        player.stop()
+        await vc.disconnect()
+    else:
+        await client.say('Non sei in un canale vocale!')
 
-	if message.content == '99!':
-		response = random.choice(brooklyn_99_quotes)
-		await message.channel.send(response)
-
-client.run('NzU5MDkwOTg3NTYwODYxNjk4.X24ckg.2ucH0GCirkLgT5wpJs-GTDWsnME')
+bot.run('NzU5MDkwOTg3NTYwODYxNjk4.X24ckg.2ucH0GCirkLgT5wpJs-GTDWsnME')
