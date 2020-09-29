ae01f6455802af50fb7452d113ec13d4f47df16f
Eros Antolloni <71837343+AntErs0@users.noreply.github.com>
2020-09-29 12:30:01 +0200

    Update shaxxbot.py

--- shaxxbot.py
+++ shaxxbot.py
@@ -9,14 +9,29 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+prefix = os.getenv('COMMAND_PREFIX')
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command('help')
 
-bot = commands.Bot(command_prefix='Shaxx ')
+@bot.event
+async def on_ready():
+	print(f'{bot.user} has connected to Discord!')
+	
+@bot.command(name="help" , pass_contex = True)
+async def help(context):    
+	embed = discord.Embed(colour = discord.Colour.from_rgb(255, 0, 0))
+	embed.set_author(name="Summon Lord Shaxx typing \"" + prefix + "\" followed by:")
+	embed.add_field(name="positive" , value= "Lord Shaxx is pleased with you" , inline=False)
+	embed.add_field(name="negative" , value= "Lord Shaxx is disappointed" , inline=False)
+	embed.add_field(name="thoughts" , value= "Random VoiP" , inline=False)
+	embed.add_field(name="no" , value= "Yassss!" , inline=False)
+	await context.send(embed=embed)
 
 @bot.event
 async def on_ready():
 	print(f'{bot.user} has connected to Discord!')
 
-@bot.command(name='positive', help='Lord Shaxx  is pleased with you',pass_context=True)
+@bot.command(name='positive', help='Lord Shaxx is pleased with you',pass_context=True)
 async def positivo(context):
 	paudiofile = [os.path.join(r,file) for r,d,f in os.walk("audio/positive") for file in f]
 	channel=None
