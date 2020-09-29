7999557fef1d5df61817ce69a104077144fbbc69
AntErs0 <71837343+AntErs0@users.noreply.github.com>
2020-09-29 11:51:58 +0200

    Update shaxxbot.py

--- shaxxbot.py
+++ shaxxbot.py
@@ -16,48 +16,63 @@ bot = commands.Bot(command_prefix='Shaxx ')
 async def on_ready():
 	print(f'{bot.user} has connected to Discord!')
 
-@bot.command(name='positive', help='Lord Shaxx is pleased with you',pass_context=True)
-async def positive(context):
-	paudiofile = os.listdir('audio/positive/')
+@bot.command(name='positive', help='Lord Shaxx  is pleased with you',pass_context=True)
+async def positivo(context):
+	paudiofile = [os.path.join(r,file) for r,d,f in os.walk("audio/positive") for file in f]
 	channel=None
 	channel = context.message.author.voice.channel
 	if channel!= None:
 		voc = await channel.connect()
-		voc.play(discord.FFmpegPCMAudio('audio/positive/' + random.choice(paudiofile)), after=lambda e: print('done', e))
+		voc.play(discord.FFmpegPCMAudio(random.choice(paudiofile)), after=lambda e: print('done', e))
 		while voc.is_playing():
 			await asyncio.sleep(1)
 		voc.stop()
 		await voc.disconnect()
 	else:
-		await context.send("You're not in any voice channel!")
+		await context.send('You are not in a voice channel!')
 
 @bot.command(name='negative', help='Lord Shaxx is disappointed',pass_context=True)
-async def negative(context):
-	paudiofile = os.listdir('audio/negative/')
+async def sconfitta(context):
+	paudiofile = [os.path.join(r,file) for r,d,f in os.walk("audio/negative") for file in f]
 	channel=None
 	channel = context.message.author.voice.channel
 	if channel!= None:
 		voc = await channel.connect()
-		voc.play(discord.FFmpegPCMAudio('audio/negative/' + random.choice(paudiofile)), after=lambda e: print('done', e))
+		voc.play(discord.FFmpegPCMAudio(random.choice(paudiofile)), after=lambda e: print('done', e))
 		while voc.is_playing():
 			await asyncio.sleep(1)
 		voc.stop()
 		await voc.disconnect()
 	else:
-		await context.send("You're not in any voice channel!")
+		await context.send('You are not in a voice channel!')
+		
+@bot.command(name='thought', help='Random message',pass_context=True)
+async def sconfitta(context):
+	paudiofile = [os.path.join(r,file) for r,d,f in os.walk("audio/") for file in f]
+	channel=None
+	channel = context.message.author.voice.channel
+	if channel!= None:
+		voc = await channel.connect()
+		voc.play(discord.FFmpegPCMAudio(random.choice(paudiofile)), after=lambda e: print('done', e))
+		while voc.is_playing():
+			await asyncio.sleep(1)
+		voc.stop()
+		await voc.disconnect()
+	else:
+		await context.send('You are not in a voice channel!')
 
-@bot.command(name='no', help='Yassss',pass_context=True)
+@bot.command(name='no', help='Yassss!',pass_context=True)
 async def Yasss(context):
 	channel=None
 	channel = context.message.author.voice.channel
 	if channel!= None:
 		voc = await channel.connect()
-		voc.play(discord.FFmpegPCMAudio('audio/yas.mp3'), after=lambda e: print('done', e))
+		voc.play(discord.FFmpegPCMAudio('audio/Yas.mp3'), after=lambda e: print('done', e))
 		while voc.is_playing():
 			await asyncio.sleep(1)
 		voc.stop()
 		await voc.disconnect()
 	else:
-		await context.send("You're not in any voice channel!")
+		await context.send('You are not in a voice channel!')
 
-bot.run('TOKEN')
+bot.run(os.getenv('DISCORD_TOKEN'))
